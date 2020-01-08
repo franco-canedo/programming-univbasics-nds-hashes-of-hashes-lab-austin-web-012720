ec9ad97928e4e43ed675593e698b0b60ffb82a99
Franco Canedo <francanedo1@hotmail.com>
2020-01-08 23:58:17 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,7 +49,11 @@ def naming_system
       :sub_category => { 
         :label => "Class",
         :sub_category => {
-          :label => "Order"
+          :label => "Order",
+          :label => "Family",
+          :sub_category => {
+            :label => "Genus"
+          }
            }
          }
       }
